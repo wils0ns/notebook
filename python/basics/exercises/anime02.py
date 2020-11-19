199f555dda243d5e06a1d1bdf82fc96ede83ad8f
Wilson Santos <wdossantosjunior@slb.com>
2020-11-19 16:50:45 +0300

    add example 2

--- python/basics/exercises/anime02.py
+++ python/basics/exercises/anime02.py
@@ -1,22 +1,36 @@
 from random import randint
 from asciimatics.screen import Screen
+from asciimatics.scene import Scene
+from asciimatics.effects import Stars
 
 
-# def background(screen):
-#     for h in height:
-#         scree
+def background(screen):
+    effects = [
+        Stars(screen, 200)
+    ]
+    screen.play([Scene(effects, 10)])
+
+
+def ship(screen):
+    center = (screen.width//2, screen.height//2)
+    screen.move(*center)
+    screen.draw(10, 10, colour=255)
 
 
 def main(screen):
-    while True:
-        screen.print_at('Hello world!',
-                        randint(0, screen.width), randint(0, screen.height),
-                        colour=randint(0, screen.colours - 1),
-                        bg=randint(0, screen.colours - 1))
-        ev = screen.get_key()
-        if ev in (ord('Q'), ord('q')):
-            return
-        screen.refresh()
+    background(screen)
+    ship(screen)
+    # while True:
+    #     background(screen)
+    #     # screen.print_at('Hello world!',
+    #     #                 randint(0, screen.width), randint(0, screen.height),
+    #     #                 colour=randint(0, screen.colours - 1),
+    #     #                 bg=randint(0, screen.colours - 1))
+    #     ev = screen.get_key()
+    #     if ev in (ord('Q'), ord('q')):
+    #         return
+    #     screen.refresh()
 
 
-Screen.wrapper(main)
+if __name__ == '__main__':
+    Screen.wrapper(main)
